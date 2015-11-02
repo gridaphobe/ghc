f2be8862b847f9c729141217a01fde663e4a54e7
Eric Seidel <gridaphobe@gmail.com>
2015-11-01 20:21:07 -0800

    document ugly corner case

--- testsuite/tests/typecheck/should_run/T10845.hs
+++ testsuite/tests/typecheck/should_run/T10845.hs
@@ -9,9 +9,18 @@ f1 = let y = (?loc :: CallStack)
      in y
 
 f2 :: (?loc :: CallStack) => CallStack
-f2 = let y x = ?loc
+f2 = let y x = (?loc :: CallStack)
+     in y 0
+
+f3 :: (?loc :: CallStack) => CallStack
+-- in this case, `y :: (?loc :: t) => a -> t`, so we don't get an entry
+-- corresponding to the occurrence of `?loc`. ugh.. i don't think
+-- there's anything we can do about this, but on the other hand, it
+-- seems *very* unlikely to occur in the real world.
+f3 = let y x = ?loc
      in y 0
 
 main :: IO ()
 main = do putStrLn $ showCallStack f1
           putStrLn $ showCallStack f2
+          putStrLn $ showCallStack f3
