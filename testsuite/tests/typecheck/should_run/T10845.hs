739c0f5957aba8a93680af0f87f306ad5dd6d64a
Eric Seidel <gridaphobe@gmail.com>
2015-11-01 20:11:48 -0800

    add another test

--- testsuite/tests/typecheck/should_run/T10845.hs
+++ testsuite/tests/typecheck/should_run/T10845.hs
@@ -1,11 +1,17 @@
 {-# LANGUAGE ImplicitParams #-}
 {-# OPTIONS_GHC -dcore-lint #-}
 
+import GHC.Exception
 import GHC.Stack
 
-f :: (?loc :: CallStack) => Int
-f = let y = getCallStack ?loc
-    in length y
+f1 :: (?loc :: CallStack) => CallStack
+f1 = let y = (?loc :: CallStack)
+     in y
+
+f2 :: (?loc :: CallStack) => CallStack
+f2 = let y x = ?loc
+     in y 0
 
 main :: IO ()
-main = print f
+main = do putStrLn $ showCallStack f1
+          putStrLn $ showCallStack f2
